7ae5f26d5b86058c5da09441679551a8ee049a44
josrph135 <oluwafemijosrph@gmail.com>
2024-03-29 09:09:25 +0100

    jade portfolio v3

--- src/contact/index.jsx
+++ src/contact/index.jsx
@@ -16,34 +16,72 @@ const Contact = () => {
           encType="multipart/form-data"
           className="text-stone-700"
         >
-          <div className=" grid md:grid-cols-2 gap-4 w-full py-2">
-            <input
-              className=" border text-xs rounded-lg pl-2 py-1 flex border-gray-300"
-              type="text"
-              placeholder="Enter Name"
-              name="name"
-            />
+          <div className=" grid md:grid-cols-2 gap-2 w-full">
+            <section className=" md:col-span-1">
+              <div className="relative pt-5">
+                <input
+                  className="peer w-full placeholder:text-transparent outline-none border sm:text-base text-sm rounded-lg pl-2 py-1 flex border-gray-300"
+                  type="text"
+                  placeholder="Enter Name"
+                  name="name"
+                  id="name"
+                />
+                <label
+                  htmlFor="name"
+                  className="pl-2 font-semibold peer-placeholder-shown:font-normal text-sm top-0 sm:text-sm text-bg1 peer-placeholder-shown:text-slate-400  peer-placeholder-shown:text-sm peer-placeholder-shown:top-6 transition-all duration-300 absolute peer-focus:top-0 peer-focus:text-bg1 peer-focus:text-sm peer-focus:font-semibold"
+                >
+                  Enter Name
+                </label>
+              </div>
 
-            <input
-              className=" border text-xs rounded-lg pl-2 py-1 flex border-gray-300"
-              type="text"
-              placeholder="Phone Number"
-              name="phone"
-            />
+              <div className="relative pt-5">
+                <input
+                  className="w-full peer placeholder:text-transparent outline-none border sm:text-base text-sm rounded-lg pl-2 py-1 flex border-gray-300"
+                  type="text"
+                  placeholder="Phone Number"
+                  name="phone"
+                />
+                <label
+                  htmlFor="ph"
+                  className="pl-2 font-semibold peer-placeholder-shown:font-normal text-sm top-0 sm:text-sm text-bg1 peer-placeholder-shown:text-slate-400  peer-placeholder-shown:text-sm peer-placeholder-shown:top-6 transition-all duration-300 absolute peer-focus:top-0 peer-focus:text-bg1 peer-focus:text-sm peer-focus:font-semibold"
+                >
+                  Phone Number
+                </label>
+              </div>
 
-            <input
-              className=" border text-xs rounded-lg pl-2 py-1 flex border-gray-300"
-              type="email"
-              placeholder="Email"
-              name="email"
-            />
-
-            <textarea
-              className="border text-xs rounded-lg pl-2 py-1 flex border-gray-300"
-              name="message"
-              rows="4"
-              placeholder="Subject"
-            ></textarea>
+              <div className="relative pt-5">
+                <input
+                  className="w-full peer placeholder:text-transparent outline-none border sm:text-base text-sm rounded-lg pl-2 py-1 flex border-gray-300"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  id="email"
+                />
+                <label
+                  htmlFor="email"
+                  className="pl-2 font-semibold peer-placeholder-shown:font-normal text-sm top-0 sm:text-sm text-bg1 peer-placeholder-shown:text-slate-400  peer-placeholder-shown:text-sm peer-placeholder-shown:top-6 transition-all duration-300 absolute peer-focus:top-0 peer-focus:text-bg1 peer-focus:text-sm peer-focus:font-semibold"
+                >
+                  Email
+                </label>
+              </div>
+            </section>
+            <section className=" md:col-span-1">
+              <div className="pt-5 relative">
+                <textarea
+                  className="w-full peer placeholder:text-transparent outline-none border sm:text-base text-sm rounded-lg pl-2 py-1 flex border-gray-300"
+                  name="message"
+                  rows="4"
+                  placeholder="Subject"
+                  id="subject"
+                ></textarea>
+                <label
+                  className="pl-2 font-semibold peer-placeholder-shown:font-normal text-sm top-0 sm:text-sm text-bg1 peer-placeholder-shown:text-slate-400  peer-placeholder-shown:text-sm peer-placeholder-shown:top-6 transition-all duration-300 absolute peer-focus:top-0 peer-focus:text-bg1 peer-focus:text-sm peer-focus:font-semibold"
+                  htmlFor="subject"
+                >
+                  Subject
+                </label>
+              </div>
+            </section>
           </div>
           <div className=" flex flex-row justify-center items-center">
             <button className=" bg-gradient-to-r from-text4 to-bg1 mt-4 w-fit text-center text-text3 text rounded px-4 py-1 whitespace-nowrap">
